16165047771e9bbd057d926f30f45f7b2ac430e4
clh910936 <carrielhunner@gmail.com>
2020-06-09 16:30:08 -0400

    created a first draft of the ClickedMovie container, created a corresponding css file

---
+++ src/components/ClickedContent/ClickedMovie.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Aux from '../../hoc/Aux/Aux';
+import './ClickedContent.module.css';
+import classes from './ClickedContent.module.css';
+
+
+
+
+const clickedMovie = ( props ) => (
+    <Aux>
+        <div>
+            <h1>{props.movie.title}</h1>
+            <table> 
+                <tbody>
+                <tr>
+                    <td>Rating: {props.movie.rating}</td>
+                    <td>Vote Count: {props.movie.vote_count}</td>
+                </tr>
+                <tr>
+                    <td>Popularity: {props.movie.popularity}</td>
+                    <td>Vote Average: {props.movie.vote_average}</td>
+                </tr>
+                <tr>
+                    <td>Format: Movie</td>
+                    <td>Original Language: {props.movie.original_language}</td>
+                </tr>
+                </tbody>
+            </table>
+            <br></br>
+            <p>Released: {props.movie.release_date}</p>
+            <p>Streaming Platforms: {props.movie.streaming_platform.join(", ")}</p>
+            <p>Production Companies: {props.movie.production_companies.join(", ")}</p>
+            <br></br>
+            <p>Overview: {props.movie.overview}</p>
+
+            
+            </div>
+    </Aux>
+);
+
+export default clickedMovie;
