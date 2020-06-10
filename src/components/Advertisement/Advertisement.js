b37b349528051af9e7959fc1786d92095b2080da
Amy Clark <akclarknp@gmail.com>
2020-06-10 11:22:09 -0400

    changes color of ad based on platform

--- src/components/Advertisement/Advertisement.js
+++ src/components/Advertisement/Advertisement.js
@@ -6,20 +6,47 @@ const advertisement = ( props ) => {
     let advertisementLink = "https://www." + props.streamingPlatform + ".com";
     if ( props.streamingPlatform === 'amazon_prime') {
         advertisementLink = "https://www.amazon.com/amazonprime";
-    }
-    
-    return (
-        <div className={classes.Advertisement}>
-            <div>
-                <a href={advertisementLink} target="_blank">
-                    <span className={classes.Uppercase}>{props.streamingPlatform}</span>
-                    <p>
-                    Click Here for 10% Off!
-                    </p>
-                </a>
+        return (
+            <div className={classes.Amazon}>
+                <div>
+                    <a href={advertisementLink} target="_blank">
+                        <span className={classes.Uppercase}>"Amazon Prime"</span>
+                        <p>
+                        Click Here for 10% Off!
+                        </p>
+                    </a>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else if (props.streamingPlatform === 'hbo') {
+      return (
+          <div className={classes.HBO}>
+              <div>
+                  <a href={advertisementLink} target="_blank">
+                      <span className={classes.Uppercase}>{props.streamingPlatform}</span>
+                      <p>
+                      Click Here for 10% Off!
+                      </p>
+                  </a>
+              </div>
+          </div>
+      );
+    } else {
+      return (
+          <div className={classes.Netflix}>
+              <div>
+                  <a href={advertisementLink} target="_blank">
+                      <span className={classes.Uppercase}>{props.streamingPlatform}</span>
+                      <p>
+                      Click Here for 10% Off!
+                      </p>
+                  </a>
+              </div>
+          </div>
+      );
+    }
+
+
 }
 
 export default advertisement;
